1d89b566cab0942ac30c899628e9b58c6441443f
Test User <test@example.com>
2025-11-13 09:33:59 +0900

    テスト　バケット

---
+++ test.py
@@ -0,0 +1,14 @@
+import boto3
+import os
+
+s3 = boto3.client(
+    "s3",
+    endpoint_url=os.getenv("AWS_ENDPOINT_URL"),
+    aws_access_key_id="test",
+    aws_secret_access_key="test",
+    region_name="us-east-1",
+)
+
+s3.create_bucket(Bucket="my-test-bucket")
+print(s3.list_buckets())
+
